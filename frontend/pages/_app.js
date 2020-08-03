6b9f5167e83d9e96a2f42ce8058b7aed9c393656
Vterebenin <terebenin.work@gmail.com>
2020-08-03 22:30:02 +0300

    feat: created default layout file

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import DefaultLayout from '../layouts/default.js'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  )
 }
 
 export default MyApp
